6074088fd604367876fef7fdd157886f52a7ea0e
Kanchan Yadav <kanchan.yadav@fugetroncorp.com>
2025-01-02 14:36:44 +0530

    variabletype learning

---
+++ java_with_dsa/com/variable_type/instance_variable/Instancevariable.java
@@ -0,0 +1,34 @@
+package com.variable_type.instance_variable;
+
+// import java.awt.image.SampleModel;
+
+public class Instancevariable {
+        // this class talks about the instance variable
+        // A variable declared inside the class but outside the method is called as instance variable
+        // the variable starts with public and you have to create a constructor class and in the main method you have to create object for that.
+        public String name;
+        public int number = 10;
+
+        private double salary;
+
+        // create a constructor to intiate the instance variable
+        public Instancevariable(){
+            this.name = "Kanchan" ;
+
+        }
+        public void Setsalary(Double usersal) {
+            salary = usersal;
+        }
+        public double getsalary() {
+            return salary;
+        }
+
+        // create a main method to create object of this class
+        public static void main(String[] args) {
+            Instancevariable obj = new Instancevariable();
+            System.out.println("this prints instance variable name: " + obj.name);
+            System.out.println("This prints instance variable number : " + obj.number);
+            obj.Setsalary(640000.00);
+            System.out.println("this is printing a private salary variable: " + obj.getsalary());
+        }
+}
---
+++ java_with_dsa/com/variable_type/local_variable/Localvariable.java
@@ -0,0 +1,12 @@
+package com.variable_type.local_variable;
+
+public class Localvariable {
+    public static void main(String[] args) {
+        // local variable:- 
+        /* variables that are assigned inside the method are called are local variable
+        * other method doesn't know about this variable other then the method 
+        * in which they are created */
+       int localvariable = 2224;
+       System.out.println("this is an exampl eof localvariable " + localvariable);
+    }
+}
